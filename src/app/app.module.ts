5aab8d3f9e95d8ca3d519cbce88213f0bec30bc3
johangarcia123 <97202290+johangarcia123@users.noreply.github.com>
2023-05-09 20:44:43 -0500

    cambis
    hola

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        PagesModule
+    ]
+})
+export class AppModule { }
